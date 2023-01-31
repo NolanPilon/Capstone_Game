3c929cb3066e30c2e87ad9e7825cb7e97ef88f49
Nolan Pilon [Student] <nolan.pilon@Student.SL.On.Ca>
2023-01-31 10:41:24 -0500

    Added start of basic movement

---
+++ Assets/Scripts/PlayerControlls.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControlls : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10;
+    public bool canJump = false;
+
+    private Rigidbody2D rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+        Jump();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground") 
+        {
+            canJump= true;
+        } 
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            canJump = false;
+        }
+    }
+
+    void PlayerMovement()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, 0);
+    }
+
+    void Jump() 
+    {
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+            Debug.Log("Jumped");
+        }
+    }
+}
+
+
