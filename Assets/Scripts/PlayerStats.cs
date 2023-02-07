c7a3c23bafd321ed086828d58ad6a6abc2fb38a8
Nolan Pilon [Student] <nolan.pilon@Student.SL.On.Ca>
2023-02-07 10:36:23 -0500

    First commit for access at home

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private int health = 3;
+    private float currentSpeed;
+    private float invincibilityFrames;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
