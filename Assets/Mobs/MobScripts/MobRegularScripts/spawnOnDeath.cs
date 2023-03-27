59376cf3f40c59cbc5244eefe74993dbbf2e2ac4
Nolan Pilon [Student] <nolan.pilon@Student.SL.On.Ca>
2023-03-27 17:11:19 -0400

    Added scorpions

--- Assets/Mobs/MobScripts/MobRegularScripts/spawnOnDeath.cs
+++ Assets/Mobs/MobScripts/MobRegularScripts/spawnOnDeath.cs
@@ -8,44 +8,33 @@ public class spawnOnDeath : MonoBehaviour
     [SerializeField] private GameObject parent;
 
     private bool isSpawned = false;
-    private bool isDead = false;
     private Vector2 spawnPosition;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (parent.transform.position.x != 1000 && parent.transform.position.y != 1000)
+        if (parent || transform.position.x != 1000 && transform.position.y != 1000)
         {
             spawnPosition = getSpawnLocation();
+            Debug.Log(spawnPosition);
         }
-        else
+        else 
         {
-            isDead = true;
-        }
-
-            if (isDead)
+            if (!isSpawned)
             {
-                 if(!isSpawned)
-                 {
-                     Instantiate(MoneyBag, spawnPosition, Quaternion.identity);
-                     print("HasSpawned");
-                     isSpawned = true;
-                 }
-
-                 Destroy(gameObject,0.2f);
+                Instantiate(MoneyBag, spawnPosition, Quaternion.identity);
+                print("HasSpawned");
+                isSpawned = true;
             }
+            Destroy(parent, 0.2f);
+        }
     }
 
     Vector2 getSpawnLocation()
     {
-
-        Vector2 spawnPosition = new Vector2(parent.transform.position.x, parent.transform.position.y);
+        if (parent) 
+        {
+            Vector2 spawnPosition = new Vector2(parent.transform.position.x, parent.transform.position.y);
+        }
 
     return spawnPosition;
     }
