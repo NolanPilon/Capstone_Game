bbc0f936b32a5258433a66d0875ae4eda17101dc
HyangsuShin <shin4873@gmail.com>
2023-03-28 08:36:24 -0400

    Update BossAI3HandR.cs

--- Assets/Scripts/Boss/BossAI3HandR.cs
+++ Assets/Scripts/Boss/BossAI3HandR.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class BossAI3HandR : MonoBehaviour
 {
+    private Vector2 initalPos;
+    private Vector2 nextPos;
+
     private void OnEnable()
     {
         StartCoroutine(StartShoot());
+        initalPos = transform.position;
+        nextPos = new Vector2(transform.position.x, transform.position.y - 3.0f);
+    }
+
+    private void Update()
+    {
+        if (BossAI3.Instance.TargetInRange(this.gameObject))
+        {
+            Movehand();
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, initalPos, 2 * Time.deltaTime);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            BossAI3.Instance.playerStat.takeDamage(collision.transform.position - this.transform.position);
+        }
     }
 
     IEnumerator StartShoot()
@@ -25,5 +51,10 @@ public class BossAI3HandR : MonoBehaviour
         rigid.velocity = dirVec * 5;
 
         Invoke("ShootParry", 1.5f);
+    }
+
+    private void Movehand()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, nextPos, 5 * Time.deltaTime);
     }
 }
