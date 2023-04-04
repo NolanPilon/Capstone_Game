ea197183df5dbed4805e4905e4479a284507ff4c
HyangsuShin <shin4873@gmail.com>
2023-04-04 08:56:32 -0400

    Romoved unusing namespaces

--- Assets/Scripts/FileSelection/InputControl.cs
+++ Assets/Scripts/FileSelection/InputControl.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
--- Assets/Scripts/FileSelection/SlotControl.cs
+++ Assets/Scripts/FileSelection/SlotControl.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class SlotControl : MonoBehaviour
