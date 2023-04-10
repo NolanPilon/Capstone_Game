a1103a28fcc27f48ac2254db89c9cc10ca4d2656
HyangsuShin <shin4873@gmail.com>
2023-04-10 14:29:32 -0400

    Update GameUI.cs

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
