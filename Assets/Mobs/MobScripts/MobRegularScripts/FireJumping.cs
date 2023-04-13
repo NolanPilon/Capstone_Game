3f5d9e463b12e9bacfc7d267f3fd4089727b6409
William Lalonde <Sir-Will03@outlook.com>
2023-04-13 18:59:03 -0400

    Added third mob

--- Assets/Mobs/MobScripts/MobRegularScripts/FireJumping.cs
+++ Assets/Mobs/MobScripts/MobRegularScripts/FireJumping.cs
@@ -3,39 +3,32 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class TumbleWeed : MonoBehaviour
+public class FireJumping : MonoBehaviour
 {
 
     [SerializeField] private Transform groundCheck = null;
     [SerializeField] private LayerMask groundLayer = 0;
-    [SerializeField] private int maxJumpForce;
-    [SerializeField] private int minJumpForce;
-    [SerializeField] private int jumpConsistentsy;
 
     private Rigidbody2D rb;
-    private int jumpForce;
+    private int jumpForce = 30;
     private bool canJump = true;
-  
+
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        OnDrawGizmos();
     }
 
     // Update is called once per frame
     void Update()
     {
-    
-        jumpForce = Random.Range(minJumpForce, maxJumpForce);
 
-        if (checkIfGround() && Random.Range(1,100) > jumpConsistentsy)
+        if (checkIfGround())
         {
-            if(canJump)
-            {
-                rb.AddForce(Vector2.up * jumpForce);
-                canJump = false;
-                StartCoroutine(delayJump());
-            }
+            rb.velocity = new Vector2(rb.velocity.x, 0);
 
+             rb.AddForce(Vector2.up * jumpForce);
+            //canJump = false;
         }
     }
 
@@ -47,9 +40,14 @@ public class TumbleWeed : MonoBehaviour
 
     IEnumerator delayJump()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
 
         canJump = true;
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundCheck.position, 0.4f);
     }
 
 }
