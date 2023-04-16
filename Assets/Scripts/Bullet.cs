6e6400bd09d5a6ea25692fe7b8cc65cfcffbd34d
William Lalonde <Sir-Will03@outlook.com>
2023-04-16 12:22:14 -0400

    proto of lvl3

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -32,7 +32,6 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("fuck");
         transform.position = new Vector2(1000, 1000);
         Destroy(gameObject, 0.1f);
     }
