40af043c887581b7b77834b10d5d8bb3bbfe1e17
HyangsuShin <shin4873@gmail.com>
2023-04-18 00:21:05 -0400

    Fixed vulture velocity non changing

--- Assets/Scripts/Boss/BossAI2Vulture.cs
+++ Assets/Scripts/Boss/BossAI2Vulture.cs
@@ -17,10 +17,20 @@ public class BossAI2Vulture : MonoBehaviour
     // Start is called before the first frame update
     private void OnEnable()
     {
-        //Initialization();
         StartCoroutine("Timer");
         this.GetComponent<BulletAttack>().ShootBullet();
-        Move();
+    }
+
+    private void Update()
+    {
+        if (BossAI2.Instance.phase == BossAI2.Phases.vulture)
+        {
+            move();
+        }
+        else if (BossAI2.Instance.phase == BossAI2.Phases.snake)
+        {
+            Initialization();
+        }
     }
 
     private void Initialization()
@@ -35,22 +45,22 @@ public class BossAI2Vulture : MonoBehaviour
         {
             dirVec = -1 * dirVec;
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
-            Move();
+            move();
         }
     }
 
-    private void Move()
+    public void move()
     {
-        rigid.velocity = dirVec.normalized * speed;
+        rigid.velocity = dirVec.normalized * 5;
     }
 
     IEnumerator Timer()
     {
         yield return new WaitForSeconds(10);
-        Initialization();
-        gameObject.SetActive(false);
+        
         BossAI2Snake.motionIndex = 3;
         BossAI2.Instance.phase = BossAI2.Phases.snake;
         BossAI2.Instance.startPhase();
+        gameObject.SetActive(false);
     }
 }
